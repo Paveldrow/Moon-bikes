42a366af5923a7f5bf427b2df338177d2f3c4e06
Pavel Drow <paveldrow@yandex.ru>
2021-06-19 00:21:21 +0300

    Добавлен подвал с контактами и картой

--- build/js/main.js
+++ build/js/main.js
@@ -2,17 +2,16 @@ const pageBody = document.querySelector('.page__body');
 pageBody.classList.remove('no-js');
 
 const buttonMenuOpen = document.querySelector('.page-header__button-menu--open');
-const menu = document.querySelector('.page-header__nav');
+const menu = document.querySelector('.main-nav');
 
 const closeMenu = function () {
   const buttonMenuClose = document.querySelector('.page-header__button-menu--close');
   buttonMenuClose.addEventListener('click', function () {
-    menu.classList.remove('page-header__nav--opened');
+    menu.classList.remove('main-nav--opened');
   });
 };
 
 buttonMenuOpen.addEventListener('click', function () {
-  menu.classList.add('page-header__nav--opened');
+  menu.classList.add('main-nav--opened');
   closeMenu();
 });
-
