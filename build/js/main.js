6c96ea8e8a7dd320faf904a0f82f311fd3de73c5
Pavel Drow <paveldrow@yandex.ru>
2021-06-21 23:11:15 +0300

    Внесение правок

--- build/js/main.js
+++ build/js/main.js
@@ -4,7 +4,6 @@ PAGEBODY.classList.remove('no-js');
 const BUTTONMENU = document.querySelector('.page-header__button-menu');
 const MENU = document.querySelector('.main-nav');
 
-
 BUTTONMENU.addEventListener('click', function () {
   MENU.classList.toggle('main-nav--opened');
   BUTTONMENU.classList.toggle('page-header__button-menu--close');
