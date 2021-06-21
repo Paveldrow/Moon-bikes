593ba739b67fbec3f262b3b0e4f5af56ab21f4f6
Pavel Drow <paveldrow@yandex.ru>
2021-06-21 11:27:53 +0300

    Добавил webP для фоновых изображений

--- gulpfile.js
+++ gulpfile.js
@@ -61,9 +61,9 @@ gulp.task("images", function () {
 });
 
 gulp.task("webp", function () {
-  return gulp.src("source/img/content/*.{png,jpg}")
+  return gulp.src("source/img/*.{png,jpg}")
     .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest("build/img/content"));
+    .pipe(gulp.dest("build/img/"));
 });
 
 gulp.task("sprite", function () {
